b43f89106e4a344a7a9f7898ec10268dbd2dca7f
Minkyu Kim <synthdnb@gmail.com>
2013-08-02 17:14:26 +0900

    push -> <<

--- 5/iterator.rb
+++ 5/iterator.rb
@@ -1,6 +1,6 @@
 str = "That's all it is: information.  Even a simulated experience or a dream; simultaneous reality and fantasy. Any way you look at it, all the information that a person accumulates in a lifetime is just a drop in the bucket."
 def rspace s
   b=0
-  s.split(/ /).map(&:length).reverse.inject([]){|r,v| r.push(s.gsub(' ','')[b...b+=v]) }.join(' ')
+  s.split(/ /).map(&:length).reverse.inject([]){|r,v| r << s.gsub(' ','')[b...b+=v] }.join(' ')
 end
 puts rspace(str)
