cf28f9d26cc0afea458ac4bd9c8580cedeca4e2a
Minwoo Lee <ermaker@gmail.com>
2013-08-02 17:11:25 +0900

    Update 5

--- 5/ermaker.rb
+++ 5/ermaker.rb
@@ -1,8 +1,5 @@
 d="succeeding you father"
 v=d.each_char.map.to_a
 y=d.delete ' '
-while i = v.index{|v| v==' '}
-  v[i] = '.'
-  y.insert -i-1, ' '
-end
+v.map.with_index.select {|v,i|v==' '}.map{|v|v[1]}.each {|i| y.insert -i-1, ' '}
 puts y
