f5ca82404cb68820e365f8215f647ab99810af6e
prudentiae <prudentiae@kaist.ac.kr>
2013-09-05 23:14:35 +0900

    Update prudentiae.c

--- 6/prudentiae.c
+++ 6/prudentiae.c
@@ -1,22 +1,23 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+void x(int a);
+void y(int a);
+
+void (*f[])(int) = {y, x};
+
 void x(int a){
   exit(0);
 }
 void y(int a){
   printf("%d\n", a);
+  f[a/1000](++a);
 }
 
 
 int main(){
   int i = 1;
-  void (*f[])(int) = {x, y}
-  
-  a:
-  f[i/1001](i);
-  i++;
-  goto a:
+  f[0](i);
   
   return 0;
 }
